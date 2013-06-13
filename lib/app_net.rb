b24255a08128d4bff96bc5ddc61978a290d9d01b
Robert Boone <robert@rlb3.com>
2013-06-13 08:30:14 -0500

    Add posts method that pulls and stashed the posts.

--- lib/app_net.rb
+++ lib/app_net.rb
@@ -3,5 +3,16 @@ require 'rest_client'
 require 'json'
 
 module AppNet
-  
+  def self.posts
+    stream = RestClient.get 'https://alpha-api.app.net/stream/0/posts/stream/global'
+    json   = JSON.parse stream
+
+    data_stash = []
+    json['data'].each do |data|
+      user = data['user']['username']
+      message = data['text']
+      data_stash << "#{user}: #{message}\n"
+    end
+    data_stash                       
+  end
 end
